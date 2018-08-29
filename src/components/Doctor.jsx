e93c052e9d5a9e3707ef2edc680ccb1b167cfb0d
Vequan23 <vequanclark@hotmail..com>
2018-08-29 17:21:31 -0500

    implemented css grid

--- src/components/Doctor.jsx
+++ src/components/Doctor.jsx
@@ -1,17 +1,22 @@
 import React, { Component } from "react";
 import Img from "../assets/images/avatar.png";
+import "../css/App.css";
 
 export default class Doctor extends Component {
   render() {
     return (
-      <div className="doctor-card">
-        <img src={Img} alt="" />
-        <h3>Susan M. Smith, MD</h3>
-        <h4>Women's Health Dermatology</h4>
-        <p>Arbor Creek</p>
-        <p>3 Miles</p>
-        <p>Olathe Health Family Medicine</p>
-        <p>10 Miles</p>
+      <div className="container">
+        <div className="doctor-card">
+          <img className="doctor-image" src={Img} alt="" />
+          <div className="credentials">
+            <h3>Susan M. Smith, MD</h3>
+            <h4>Women's Health Dermatology</h4>
+          </div>
+          <div className="location">
+            <p>Arbor Creek</p>
+            <p>3 Miles</p>
+          </div>
+        </div>
       </div>
     );
   }
